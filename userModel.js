74425f463a05f44639b664495a230460470b71f2
aatifnehal786 <84582857+aatifnehal786@users.noreply.github.com>
2024-11-16 20:29:01 +0530

    Add files via upload

---
+++ userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:[true,"Name is Mandatory"]
+    },
+    email:{
+        type:String,
+        required:[true,"Email is Mandatory"]
+    },
+    password:{
+        type:String,
+        required:[true,"password is Mandatory"]
+    },
+    age:{
+        type:Number,
+        required:[true,"Age is Mandatory"],
+        min: 12
+    }
+},{timestamps:true})
+
+const userModel = mongoose.model("users",userSchema)
+
+module.exports = userModel;
